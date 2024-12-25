84e85aa63cc8ecb4097f7644ceeb9bd7337173e9
Prasannakumara <153364276+Prasannakumara@users.noreply.github.com>
2024-12-25 12:43:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "uy5GHSbD#4mkalwMWyzFTMkBFfK7XwakG51Eq-DAu2tDeEiZMT7o", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "GqYUgLiL#LosQ_TcFUd_fZpNNXtNoaAjDzqSwaFRpCPKghASXO0c", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
